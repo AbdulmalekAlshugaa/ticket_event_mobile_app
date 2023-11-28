e593a61d1ff76cb809607a31c2d929e026452dbb
abdulmalik alshugaa <alshugaa19@gmail.com>
2023-11-29 00:20:06 +0700

    feat(details): create details screen page and render the required data

---
+++ app/components/index.ts
@@ -0,0 +1,6 @@
+import AppBodyText from "./AppBodyText";
+import AppButton from "./AppButton";
+import AppBoldText from "./AppBoldText";
+import AppRatting from "./AppRatting";
+
+export { AppBodyText, AppButton, AppBoldText, AppRatting };
