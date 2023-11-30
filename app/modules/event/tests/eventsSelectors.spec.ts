25988d0e16ab7be2c251da574bbc3527dcb163c7
abdulmalik alshugaa <alshugaa19@gmail.com>
2023-11-30 21:59:19 +0700

    feat(unitTest): adding unit test to the product

--- app/modules/event/tests/eventsSelectors.spec.ts
+++ app/modules/event/tests/eventsSelectors.spec.ts
@@ -1,1 +1,74 @@
-//describe('Test :: event selectors Selectors', () => {});
+import {
+    eventSelector,
+    getEventsSelector,
+    isLoadingSelector,
+    errorMessagesSelector,
+    getTotalPagesSelector,
+} from '../src/eventSelectors';
+describe('Test :: event selectors Selectors', () => {
+    test('should select the feature state', () => {
+        const result = eventSelector({
+            eventsDiscovery: {
+                events: {},
+                eventFilter: {},
+            },
+        });
+        expect(result).toEqual({});
+    });
+
+    test('should select the events', () => {
+        const result = getEventsSelector({
+            eventsDiscovery: {
+                events: {
+                    data: {
+                        _embedded: {
+                            events: [],
+                        },
+                    },
+                },
+                eventFilter: {},
+            },
+        });
+        expect(result).toEqual([]);
+    });
+
+    test('should select the isLoading', () => {
+        const result = isLoadingSelector({
+            eventsDiscovery: {
+                events: {
+                    isLoading: false,
+                },
+                eventFilter: {},
+            },
+        });
+        expect(result).toEqual(false);
+    });
+
+    test('should select the errorMessages', () => {
+        const result = errorMessagesSelector({
+            eventsDiscovery: {
+                events: {
+                    problem: '',
+                },
+                eventFilter: {},
+            },
+        });
+        expect(result).toEqual('error');
+    });
+
+    test('should select the getTotalPages', () => {
+        const result = getTotalPagesSelector({
+            eventsDiscovery: {
+                events: {
+                    data: {
+                        page: {
+                            totalPages: 0,
+                        },
+                    },
+                },
+                eventFilter: {},
+            },
+        });
+        expect(result).toEqual(0);
+    });
+});
