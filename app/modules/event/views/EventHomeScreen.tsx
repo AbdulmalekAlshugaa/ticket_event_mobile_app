e5e93ce49238ce1a4477ee7abc664208b7760e6e
abdulmalik alshugaa <alshugaa19@gmail.com>
2023-12-03 00:00:27 +0700

    feat(event): render the first three sections

---
+++ app/modules/event/views/EventHomeScreen.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+// TODO: we have to have the same search section 
+// TODO: we have to have the same filter section
+// TODO: we have to have a section to render an and render the first three events
+ const  EventHomeScreen = ()=> {
+  return (
+    <View>
+      <Text>EventHomeScreen</Text>
+    </View>
+  )
+}
+
+export default EventHomeScreen;
